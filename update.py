ff463eebd5fee9a9cc1658d2467946ac843fb1b6
Bryan Iddings <bryan@iddings.co>
2018-08-02 14:23:30 -0400

    add install file

--- update.py
+++ update.py
@@ -1,9 +1,12 @@
 from shutil import rmtree, copytree
 from pathlib import Path
 from os import path
+from subprocess import run
 
 cd = Path(path.dirname(__file__))
 
+run([r"C:\Program Files\Git\git-bash.exe", "-c", "git pull"])
+
 rmtree(cd / 'src')
-copytree(cd / 'dist', cd)
+copytree(cd / 'dist/ecqemailmanager', cd)
 rmtree(cd / 'dist')
