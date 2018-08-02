1005034677e4e54054696d21ec2447de28b28718
Bryan Iddings <bryan@iddings.co>
2018-08-02 14:34:29 -0400

    update update.py

--- deploy.py
+++ deploy.py
@@ -1,12 +1,13 @@
 from shutil import rmtree, copytree
 from pathlib import Path
-from os import path
+from os import path, unlink
 from subprocess import run
 
 cd = Path(path.dirname(__file__))
 
 run([r"C:\Program Files\Git\git-bash.exe", "-c", "git fetch --all && git reset --hard origin/master"])
 
+unlink('build.py')
 rmtree(cd / 'src')
 rmtree(cd / 'ecqemailmanager')
 copytree(cd / 'dist/ecqemailmanager', cd / 'ecqemailmanager')
