45bd4ef9d90892d9307692a55a3bdffa842e4275
Bryan Iddings <bryan@iddings.co>
2018-08-02 14:28:33 -0400

    add install file

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 from shutil import rmtree, copytree
 from pathlib import Path
-from os import path
+from os import path, rename
 from subprocess import run
 
 cd = Path(path.dirname(__file__))
@@ -8,5 +8,6 @@ cd = Path(path.dirname(__file__))
 run([r"C:\Program Files\Git\git-bash.exe", "-c", "git fetch --all && git reset --hard origin/master"])
 
 rmtree(cd / 'src')
-copytree(cd / 'dist/ecqemailmanager', cd)
+copytree(cd / 'dist/ecqemailmanager', cd / 'tmp')
 rmtree(cd / 'dist')
+rename(cd / 'tmp', cd / 'ecqemailmanager')
