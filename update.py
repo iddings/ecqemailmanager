f48629da514bed91136b6677cba02b14aa22d997
Bryan Iddings <bryan@iddings.co>
2018-08-02 17:02:20 -0400

    add inline venv activation

--- update.py
+++ update.py
@@ -1,4 +1,6 @@
 from subprocess import run
 
 run([r"C:\Program Files\Git\git-bash.exe", "-c", "git fetch --all && git reset --hard origin/master"])
-run(["python", "-m", "pipenv", "run", "python", "deploy.py"])
+
+with open("deploy.py") as fp:
+    exec(fp.read())
