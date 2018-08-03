d7b273410d0aa7b9cab7b534f70914c1d7f28132
Bryan Iddings <bryan@iddings.co>
2018-08-03 16:39:27 -0400

    make concurrent executions configurable

--- dist/ecqemailmanager/ecq.py
+++ dist/ecqemailmanager/ecq.py
@@ -8,7 +8,7 @@ from .models import Macro, MacroTask
 from threading import BoundedSemaphore
 
 
-exec_lock = BoundedSemaphore(value=2)
+exec_lock = BoundedSemaphore(value=config.ecq_concurrent_executions)
 
 log = logging.getLogger(__name__)
 
